3c5547c251f081068d3b012785541d6facc768bd
Alex Semyonov <al@semyonov.us>
2016-03-28 04:17:01 +0300

    Remove fucking stupid spec

--- spec/fucktherubyracer_spec.rb
+++ spec/fucktherubyracer_spec.rb
@@ -4,8 +4,4 @@ describe Fucktherubyracer do
   it 'has a version number' do
     expect(Fucktherubyracer::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
